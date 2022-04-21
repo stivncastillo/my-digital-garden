2eee214bc10974043b7de9d17afd7a608d4fd15d
Stiven <stivencastillo.90@gmail.com>
2022-04-21 11:54:10 -0500

    Add google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,40 @@
 import '../styles/globals.css'
+import React, { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import Container from '../components/Layout/Container'
 import Navbar from '../components/Layout/Navbar'
 import Footer from '../components/Layout/Footer'
+import { useRouter, Router } from 'next/router'
+
+declare global {
+  interface Window {
+    dataLayer: any
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const pageView = (url: string, title: string) => {
+    window &&
+      window.dataLayer &&
+      window.dataLayer.push({
+        event: 'virtualPageview',
+        virtualPageURL: url,
+        virtualPageTitle: title,
+      })
+  }
+
+  useEffect(() => {
+    pageView(router.pathname, document.title)
+    const handleRouteChange = (url: string) => {
+      pageView(url, document.title)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
   return (
     <ThemeProvider attribute="class">
       <Container>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,19 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#6366F1" />
           <meta name="google" content="notranslate" />
           <meta name="robots" content="all" />
+
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');`,
+            }}
+          />
+          {/* End Google Tag Manager */}
+
           <link
             href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@300;400;500;700&family=Open+Sans:wght@300;400;700&display=swap"
             rel="stylesheet"
@@ -23,6 +36,11 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
           <Main />
           <NextScript />
         </body>
