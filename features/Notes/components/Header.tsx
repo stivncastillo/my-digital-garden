dc659c750126dee311f938310c9c4ca70d345970
Stiven <stivencastillo.90@gmail.com>
2022-09-18 22:13:36 -0500

    Change note description

--- features/Notes/components/Header.tsx
+++ features/Notes/components/Header.tsx
@@ -3,12 +3,11 @@ import React from 'react'
 const Header: React.VFC = () => {
   return (
     <>
-      <h1 className="text-3xl mb-2">Notes</h1>
+      <h1 className="text-3xl mb-2">Notas - Lo que hoy aprendí</h1>
 
-      <p className="text-slate-500 dark:text-slate-300 font-thin text-xl mb-4">
-        Bienvenidos a mis notas, recortes y bookmarks que adornan mi Digital
-        Garden. Este espacio lo uso para anotar y compartir cosas que me ayudan
-        en mi día a día como desarrollador.
+      <p className="text-slate-500 dark:text-slate-300 font-thin text-xl mb-8">
+        Esta es una colección de notas cortas de cosas que he aprendido desde
+        nuevas técnologias, snippets hasta ideas del día.
       </p>
       <p className="text-sm text-slate-400 dark:text-slate-500">
         Disfutenlo y cuidado me pisa el césped!
